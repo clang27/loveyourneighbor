9049231ee1ec2769c6328aa42f71dbc0225c4198
clang27 <collinlang0@gmail.com>
2020-07-09 19:59:40 -0400

    Sets up react application

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
-FROM node:12
+FROM node:12 as build
 WORKDIR /usr/src/app
-COPY package*.json ./
-RUN npm install
+ENV PATH /usr/src/app/node_modules/.bin:$PATH
+COPY ./package*.json ./
+RUN npm install --silent
+RUN npm install react-scripts react-scripts@3.4.1 -g --silent
 COPY . .
+RUN npm run build
+
+FROM httpd:2.4
+COPY --from=build /usr/src/app/build /usr/local/apache2/htdocs/
 EXPOSE 8080
-CMD [ "node", "server.js" ]
--- startDocker.sh
+++ startDocker.sh
@@ -5,4 +5,17 @@ IMAGE_NAME="loveyourneighbor"
 TAG="v0"
 
 sudo docker stop $IMAGE_NAME &> /dev/null && sudo docker rm $IMAGE_NAME &> /dev/null
-sudo docker build -t $IMAGE_NAME:$TAG . && sudo docker run -dit --name $IMAGE_NAME -p $DESKTOP_PORT:8080 $IMAGE_NAME:$TAG
+
+sudo docker build \
+    -t $IMAGE_NAME:$TAG \
+    .
+    
+sudo docker run \
+    -dit \
+    --rm \
+    -v ${PWD}:/usr/src/app \
+    -v /usr/src/app/node_modules \
+    --name $IMAGE_NAME \
+    -p $DESKTOP_PORT:8080 \
+    -e CHOKIDAR_USEPOLLING=true \
+    $IMAGE_NAME:$TAG
